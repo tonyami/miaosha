9d1d132bc0243109a7ec8fade5e593008e55d759
tony <tony@mtony.cn>
2021-02-23 21:32:23 +0800

    修复context deadline exceeded

--- internal/rdb/redis.go
+++ internal/rdb/redis.go
@@ -91,7 +91,7 @@ func ZRem(key string, member interface{}) (err error) {
 }
 
 func ZRangeByScore(key, min, max string, offset, count int64) (list []string, err error) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	z := &redis.ZRangeBy{
 		Min:    min,
 		Max:    max,
